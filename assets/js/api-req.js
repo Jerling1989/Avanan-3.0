37f0d3198aa31b55feeea53c2aa5f04ba05ea56a
Jacob <jerling1989@gmail.com>
2019-06-14 14:20:02 -0400

    api file

---
+++ assets/js/api-req.js
@@ -0,0 +1,18 @@
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var request = new XMLHttpRequest()
+// Open a new connection, using the GET request on the URL endpoint
+// request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+request.open('GET', 'https://u7q76ggsff.execute-api.us-east-1.amazonaws.com/production', true)
+request.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response)
+  var emails = data['Total Emails/Files Scanned by Avanan'];
+  var phishing = data['Phishing Attacks Blocked By Avanan'];
+  var malware = data['Malware Attacks Blocked By Avanan'];
+  // console.log(data);
+  console.log('Emails Scanned: ' + emails);
+  console.log('Phishing: ' + phishing);
+  console.log('Malware: ' + malware);        
+}
+// Send request
+request.send()
